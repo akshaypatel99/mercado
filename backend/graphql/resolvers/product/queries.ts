669daa1c5afcadd05ad21d57161a3099d2e0610f
akshaypatel99 <akshaypatel99@gmail.com>
2022-01-22 16:43:05 +0000

    Add pagination to queries

--- backend/graphql/resolvers/product/queries.ts
+++ backend/graphql/resolvers/product/queries.ts
@@ -3,7 +3,30 @@ import { Product } from '../../../db/models'
 const productQueries = {
   products: async (parent, args, context) => {
     try {
-      return await Product.find()
+      const { pageSize, page } = args.params;
+
+      return {
+        results: async () => {
+          const products = await Product.find()
+            .skip(pageSize * (page - 1))
+            .limit(pageSize);
+          
+          return products;
+        },
+        info: async () => {
+          const count = await Product.countDocuments();
+          const pages = Math.ceil(count / pageSize);
+          const prev = page > 1 ? page - 1 : null;
+          const next = page < pages ? page + 1 : null;
+
+          return {
+            count,
+            pages,
+            prev,
+            next
+          }
+        }
+      }
     } catch (error) {
       return error
     }
--- backend/graphql/resolvers/user/queries.ts
+++ backend/graphql/resolvers/user/queries.ts
@@ -3,7 +3,30 @@ import { User } from "../../../db/models";
 const userQueries = {
   users: async (parent, args, context) => {
     try {
-      return await User.find()
+      const { pageSize, page } = args.params;
+
+      return {
+        results: async () => {
+          const users = await User.find()
+            .skip(pageSize * (page - 1))
+            .limit(pageSize);
+          
+          return users;
+        },
+        info: async () => {
+          const count = await User.countDocuments();
+          const pages = Math.ceil(count / pageSize);
+          const prev = page > 1 ? page - 1 : null;
+          const next = page < pages ? page + 1 : null;
+
+          return {
+            count,
+            pages,
+            prev,
+            next
+          }
+        }
+      }
     } catch (error) {
       return error
     }
