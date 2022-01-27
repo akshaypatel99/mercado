deb0420bab41e3bb483a2e40d4ca01eff4e14aa4
akshaypatel99 <akshaypatel99@gmail.com>
2022-01-27 15:09:29 +0000

    Add env variables

--- frontend/apollo-client.ts
+++ frontend/apollo-client.ts
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "https://localhost:8080/graphql",
+  uri: process.env.REACT_APP_GRAPHQL_URI,
   cache: new InMemoryCache()
 });
 
