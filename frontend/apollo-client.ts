65a8424e731a64aaa0601cf067c284e901c48815
akshaypatel99 <akshaypatel99@gmail.com>
2022-01-31 23:06:50 +0000

    Add apollo-upload-client

--- frontend/apollo-client.ts
+++ frontend/apollo-client.ts
@@ -1,8 +1,26 @@
-import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, from, InMemoryCache } from '@apollo/client';
+import { onError } from "@apollo/client/link/error";
+import { createUploadLink } from 'apollo-upload-client';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const uploadLink = createUploadLink({
+  uri: process.env.REACT_APP_GRAPHQL_URI,
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  })
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URI,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: from([errorLink, uploadLink]) 
 });
 
 export default client;
--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -2,11 +2,19 @@ import Link from 'next/link';
 
 export default function Nav() {
 	return (
-		<nav className='grid grid-cols-4 font-bold text-xl my-2 uppercase'>
-			<Link href='/products'>Products</Link>
-			<Link href='/sell'>Sell</Link>
-			<Link href='/orders'>Orders</Link>
-			<Link href='/account'>Account</Link>
+		<nav className='grid grid-cols-4 font-bold text-xl my-2 uppercase '>
+			<Link href='/products'>
+				<a className='hover:text-teal-500'>Products</a>
+			</Link>
+			<Link href='/sell'>
+				<a className='hover:text-teal-500'>Sell</a>
+			</Link>
+			<Link href='/orders'>
+				<a className='hover:text-teal-500'>Orders</a>
+			</Link>
+			<Link href='/account'>
+				<a className='hover:text-teal-500'>Account</a>
+			</Link>
 		</nav>
 	);
 }
