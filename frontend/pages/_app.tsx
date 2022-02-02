4ce821bf19d169e372b9996e8061dec248d63df4
akshaypatel99 <akshaypatel99@gmail.com>
2022-02-02 23:27:50 +0000

    Update Apollo Client to handle cookies

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import { ApolloClient, from, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { createUploadLink } from 'apollo-upload-client';
 import NProgress from 'nprogress';
 import Router from 'next/router';
-import client from '../apollo-client';
 import Page from '../components/Page';
 import '../styles/globals.css';
 import 'nprogress/nprogress.css';
@@ -10,6 +12,28 @@ import 'nprogress/nprogress.css';
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors)
+		graphQLErrors.forEach(({ message, locations, path }) =>
+			console.log(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			)
+		);
+
+	if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const uploadLink = createUploadLink({
+	uri: process.env.REACT_APP_GRAPHQL_URI,
+	credentials: 'include',
+});
+
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	credentials: 'include',
+	link: from([errorLink, uploadLink]),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
