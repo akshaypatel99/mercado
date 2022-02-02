48e02664e9c97f6d83e47b77db6dcf44c59a885e
akshaypatel99 <akshaypatel99@gmail.com>
2022-02-02 20:38:36 +0000

    Add cors, cookie and token middleware

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import http from 'http';
 import { ApolloServer } from 'apollo-server-express';
 import { graphqlUploadExpress } from 'graphql-upload';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import validateTokensMiddleware from './middleware/validateTokens';
 import { port } from './config/environment';
 import schema from './graphql/';
 import connectDB from './db';
@@ -13,19 +16,23 @@ async function startApolloServer(schema) {
   const server = new ApolloServer({
     schema,
     introspection: true,
-    context: ({ res }) => ({
-      res
-    })
+    context: ({ req, res }) => ({ req, res }),
   });
   
   await server.start();
+
+  const corsOptions = {
+    credentials: true,
+    origin: [process.env.DEVELOPMENT_URL, process.env.PRODUCTION_URL, process.env.APOLLO_URL]
+  };
+
+  app.use(cors(corsOptions))
+  app.use(cookieParser());
+  app.use(validateTokensMiddleware);
   app.use(graphqlUploadExpress());
   server.applyMiddleware({
     app,
-    cors: {
-      credentials: true,
-      origin: ['http://localhost:3000', 'https://studio.apollographql.com']
-    }
+    cors: false
   });
 
   await connectDB();
